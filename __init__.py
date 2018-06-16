1168ca0c5b82f44f333d2feac59430729e4f2493
sdlm <sdlmerr@yandex.ru>
2018-06-16 20:58:52 +0300

    move grabber to separate repo

--- __init__.py
+++ __init__.py
@@ -1,5 +1,2 @@
-from rss.classes import *
-from file.csv import *
-from file.json import *
 from fields import *
 from schema import *
--- abstract_classes.py
+++ abstract_classes.py
@@ -1,15 +1,4 @@
 import abc
-from typing import Union
-
-
-class Grabber(abc.ABC):
-
-    @abc.abstractmethod
-    def load_data(self) -> Union[dict, list]:
-        """
-        Получить данные из внешнего источника.
-        """
-        pass
 
 
 class Field(abc.ABC):
